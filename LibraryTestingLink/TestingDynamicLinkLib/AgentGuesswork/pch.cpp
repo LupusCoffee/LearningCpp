66562228c272d410b00c50402ef699e691e830c1
LupusCoffee <96193514+LupusCoffee@users.noreply.github.com>
2025-03-25 15:31:49 +0100

    created a library

--- LibraryTestingLink/TestingDynamicLinkLib/AgentGuesswork/pch.cpp
+++ LibraryTestingLink/TestingDynamicLinkLib/AgentGuesswork/pch.cpp
@@ -3,3 +3,10 @@
 #include "pch.h"
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
+
+#include <vector>
+extern "C" __declspec(dllexport) //__declspec(dllexport) exportes the function to be used in the DLL, making it accessible to other programs! "extern 'C'" prevents C++ name mangling
+int GetRandNum(int min, int max)
+{
+	return rand() % (max - min + 1) + min;
+}
